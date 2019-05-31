962339e09f1aa630cab4280b6aa4a44799d4a8de
Mt-cli <b1013150@fun.ac.jp>
2019-05-31 15:28:09 +0900

    make ver2

---
+++ neko/move_ball_ver2.0.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+import os, sys, time
+import random
+import math
+
+
+# 処理部分
+def calc_canvas():
+    return anko
+
+# 描画画面キャンバス
+def main_screen():
+    return 0
+
+
+# 設定画面キャンバス
+def setting_screen():
+    return inko
+
+
+# 変更の際にアップデート
+
+# スタート
+root=tk.Tk()
+screen01=Main_screen()
+
+while 1:
+
+
+def Main_screen()
+root.update()
